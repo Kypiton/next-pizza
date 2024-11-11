dcbf619ddc50c8baa72b82708f66f59c683899b1
Dan <partytimeyep@gmail.com>
2024-11-11 11:55:21 +0200

    next pizza with some components

---
+++ components/shared/index.ts
@@ -0,0 +1,6 @@
+export { Categories } from './categories';
+export { SortPopup } from './sort-popup';
+export { Container } from './container';
+export { TopBar } from './top-bar';
+export { Header } from './header';
+export { Title } from './title';
